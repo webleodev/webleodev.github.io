9de77a33c91e4bf6eebb569a2495f39a69909c96
ITDev-front <web.leo.develop@gmail.com>
2020-09-11 23:20:57 +0300

    Added mobile styles and updated HTML doc

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,7 @@
-// Requirements
-
 let gulp = require('gulp'),
     sass = require('gulp-sass'),
     browserSync = require('browser-sync'),
     rename = require('gulp-rename');
-
-// Compressed styles
 
 gulp.task('scss-min', () => {
     return gulp.src('./styles/styles.scss')
@@ -31,8 +27,6 @@ gulp.task('mobile-scss-min', () => {
         .pipe(browserSync.reload({stream: true}))
 });
 
-// Normal styles
-
 gulp.task('scss-normal', () => {
     return gulp.src('./styles/styles.scss')
         .pipe(sass({outputStyle: 'expanded'}))
@@ -54,21 +48,15 @@ gulp.task('mobile-scss-normal', () => {
         .pipe(browserSync.reload({stream: true}))
 });
 
-// On change reload html-doc
-
 gulp.task('html', () => {
     return gulp.src('./*.html')
         .pipe(browserSync.reload({stream: true}))
 });
 
-// On change reload scripts
-
 gulp.task('script', () => {
     return gulp.src('./*.js')
         .pipe(browserSync.reload({stream: true}))
 });
-
-// Reloading browser on changes at the project's directory
 
 gulp.task('browser-sync', () => {
     browserSync.init({
@@ -78,22 +66,13 @@ gulp.task('browser-sync', () => {
     });
 });
 
-// Task watching for changes in files
-
-gulp.task('watch', () => {
-
-    // Watching for 3 style files in project
-    
+gulp.task('watch', () => {  
     gulp.watch('./styles/styles.scss', gulp.parallel('scss-min', 'scss-normal'));
     gulp.watch('./styles/media.scss', gulp.parallel('media-scss-min', 'media-scss-normal'));
     gulp.watch('./styles/mobile.scss', gulp.parallel('mobile-scss-min', 'mobile-scss-normal'));
 
-    // Watching for any HTML and JS files in project
-
     gulp.watch('./*.html', gulp.parallel('html'));
     gulp.watch('./*.js', gulp.parallel('script'));
 });
-
-// Default command for gulp
 
 gulp.task('default', gulp.parallel('scss-min', 'media-scss-min', 'mobile-scss-min', 'scss-normal', 'media-scss-normal', 'mobile-scss-normal', 'script', 'html', 'browser-sync', 'watch'))
